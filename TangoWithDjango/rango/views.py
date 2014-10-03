28aa18e6d14e20469c2f5ecc5743fcbb1ea04e10
Ram <ramprasathdgl@gmail.com>
2014-10-03 21:53:24 +0530

    Tutorial 3: Django Basics Visit the link http://www.tangowithdjango.com/book/chapters/setup.html for details

--- TangoWithDjango/rango/views.py
+++ TangoWithDjango/rango/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    return HttpResponse("Rango says: Hello world! <a href='/rango/about'>About</a>")
+
+def about(request):
+	return HttpResponse("<a href='/rango/'>Index</a>")
+
+
+
