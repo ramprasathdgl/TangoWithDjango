8ce62d790f3139db11260c414f26c15865f55fa6
Ram <ramprasathdgl@gmail.com>
2014-10-03 22:52:02 +0530

    Tutorial 4 : Templates and Static Files

--- TangoWithDjango/TangoWithDjango/settings.py
+++ TangoWithDjango/TangoWithDjango/settings.py
@@ -76,8 +76,19 @@ USE_L10N = True
 
 USE_TZ = True
 
+#Templates Directory
+TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
-
+STATIC_PATH = os.path.join(BASE_DIR,'static')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    STATIC_PATH,
+)
+
+#Media Files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # Absolute path to the media directory
+
+
--- TangoWithDjango/rango/views.py
+++ TangoWithDjango/rango/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import RequestContext
+from django.shortcuts import render_to_response
 
 def index(request):
-    return HttpResponse("Rango says: Hello world! <a href='/rango/about'>About</a>")
+	context         = RequestContext(request)
+	context_dict    = {'boldmessage':"I am bold font"}
+	return render_to_response('rango/index.html', context_dict, context)
+    #return HttpResponse("Rango says: Hello world! <a href='/rango/about'>About</a>")
 
 def about(request):
-	return HttpResponse("<a href='/rango/'>Index</a>")
+	context         = RequestContext(request)
+	context_dict    = {'boldmessage':"I am bold font"}
+	return render_to_response('rango/about.html', context_dict, context)
+	#return HttpResponse("<a href='/rango/'>Index</a>")
 
 
 
