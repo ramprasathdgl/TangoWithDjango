87c76ff551aa682fdb461d6c2a6c5e15028b633c
Ram <ramprasathdgl@gmail.com>
2014-10-04 22:25:39 +0530

    Tutorial 5 : Models, DataBases, Admin Added admin interface with usetname : ram and password:commandos For details visit the page http://www.tangowithdjango.com/book/chapters/models.html

--- TangoWithDjango/rango/views.py
+++ TangoWithDjango/rango/views.py
@@ -2,18 +2,22 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import RequestContext
 from django.shortcuts import render_to_response
+from rango.models import Category
 
 def index(request):
-	context         = RequestContext(request)
-	context_dict    = {'boldmessage':"I am bold font"}
-	return render_to_response('rango/index.html', context_dict, context)
+	#context         = RequestContext(request)
+	category_list   = Category.objects.order_by('likes')[:5]
+	context_dict    = {'categories':category_list}
+	#return render_to_response('rango/index.html', context_dict, context)
+	return render(request,'rango/index.html',context_dict)
     #return HttpResponse("Rango says: Hello world! <a href='/rango/about'>About</a>")
 
 def about(request):
-	context         = RequestContext(request)
+	#context         = RequestContext(request)
 	context_dict    = {'boldmessage':"I am bold font"}
-	return render_to_response('rango/about.html', context_dict, context)
+	#return render_to_response('rango/about.html', context_dict, context)
 	#return HttpResponse("<a href='/rango/'>Index</a>")
+	return render(request,'rango/about.html',context_dict)
 
 
 
